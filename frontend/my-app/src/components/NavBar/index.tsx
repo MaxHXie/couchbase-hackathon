a3742386769f6284ab79a9b275c680ce33d5ac74
Otkur <95427785+otkurA@users.noreply.github.com>
2023-05-27 15:42:19 +0200

    progress on landing page

--- frontend/my-app/src/components/NavBar/index.tsx
+++ frontend/my-app/src/components/NavBar/index.tsx
@@ -1,40 +1,28 @@
-import { useRef } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import "./style.scss"
 import PrimaryButton from "../buttons/PrimaryButton"
-import SearchComponent from "../SearchComponent"
+import SiteIcon from "../../static/icons/siteIcon.svg"
 
 const NavBar = () => {
-  const navigate = useNavigate()
-
-  const searchOnSubmit = (searchInputElement: string) => {}
-  const navbarSearchRef = useRef<HTMLDivElement>(null)
-
   return (
     <nav className="standardPaddingSection">
-      <div className="navBarLeftItems">
+      <Link to="/">
+        <img src={SiteIcon} alt="icon" />
+      </Link>
+      <div className="navBarLink">
         <Link to="/">
           <PrimaryButton variant="textVariantA naked underlineHover">
-            Blinket
+            Home
           </PrimaryButton>
         </Link>
-        <div className="navBarLink">
-          <>
-            <Link to="/">
-              <PrimaryButton variant="textVariantA naked underlineHover">
-                Home
-              </PrimaryButton>
-            </Link>
-            <Link to="/events">
-              <PrimaryButton
-                variant="textVariantA naked underlineHover"
-                cy="marketplaceNavbarItem"
-              >
-                Events
-              </PrimaryButton>
-            </Link>
-          </>
-        </div>
+        <Link to="/events">
+          <PrimaryButton
+            variant="textVariantA naked underlineHover"
+            cy="marketplaceNavbarItem"
+          >
+            Events
+          </PrimaryButton>
+        </Link>
       </div>
     </nav>
   )
