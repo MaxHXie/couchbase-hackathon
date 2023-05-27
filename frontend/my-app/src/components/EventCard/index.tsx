bd88e3e9754ee0a11d88eae2a45e6928cd55d76a
Otkur <95427785+otkurA@users.noreply.github.com>
2023-05-27 16:40:11 +0200

    commit

--- frontend/my-app/src/components/EventCard/index.tsx
+++ frontend/my-app/src/components/EventCard/index.tsx
@@ -1,8 +1,19 @@
 import React from "react"
 import "./style.scss"
-
+import EventImage from "../../static/icons/eventImg.png"
 const EventCard = () => {
-  return <div className="cardContainer">EventCard</div>
+  return (
+    <div className="cardContainer">
+      <div className="title"></div>
+      <div className="image">
+        <img src={EventImage} alt="" />
+      </div>
+      <div className="titleAndState">
+        <div className="dateTime">Date and Time</div>
+        <div className="availability">Ticket Available</div>
+      </div>
+    </div>
+  )
 }
 
 export default EventCard
